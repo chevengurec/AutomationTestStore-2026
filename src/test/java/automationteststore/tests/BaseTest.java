e21585b87b066b4833c2dd8456765fcc1ba44a4b
chevengurec <evgeniyperevalov@gmail.com>
2026-04-24 10:47:11 +0300

    Еще изменения

--- src/test/java/automationteststore/tests/BaseTest.java
+++ src/test/java/automationteststore/tests/BaseTest.java
@@ -8,9 +8,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -44,8 +47,22 @@ public class BaseTest {
 
         driver.get(config.homepageUrl());
 
+        WebDriverWait pageWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        pageWait.until(webDriver ->
+                ((org.openqa.selenium.JavascriptExecutor) webDriver)
+                        .executeScript("return document.readyState")
+                        .equals("complete")
+        );
+
         waitHelper = new WaitHelper(driver, 10);
+
+        pageWait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("body")));
+
+        waitHelper = new WaitHelper(driver, 10);
+
+        waitHelper.shortDelay();
     }
+
 
     @AfterEach
     public void tearDown() {
